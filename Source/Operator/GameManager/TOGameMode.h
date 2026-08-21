fbf82d902fa247bc05c9cc59b5965239efaba669
chanchan0810 <imchanwoo5302@gmail.com>
2026-08-21 16:24:38 +0900

    폴더 분배 및 기본 C++파일 생성, Build.cs 에 UMG,Slate,SlateCore 추가, Project Setting - Default Level = OpenWorld -> NewWorld로 변경

---
+++ Source/Operator/GameManager/TOGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TOGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPERATOR_API ATOGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Operator/Interactive/TOChair.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TOChair.h"
+
+// Sets default values
+ATOChair::ATOChair()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATOChair::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATOChair::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
