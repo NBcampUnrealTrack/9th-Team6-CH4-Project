c02b266bf625847c33451866d9b53fb9a6a81926
gyeongsookim <daesin1021@naver.com>
2026-08-24 20:56:01 +0900

    ATOCharacter 착석 시스템, 모듈러 메시 동기화, 손 소켓 부착 및 이모트 멀티캐스트 구현

--- Source/Operator/Character/TOCharacter.cpp
+++ Source/Operator/Character/TOCharacter.cpp
@@ -1,34 +1,168 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TOCharacter.h"
+#include "Net/UnrealNetwork.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Animation/AnimMontage.h"
+#include "Animation/AnimInstance.h"
 
-// Sets default values
 ATOCharacter::ATOCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	bReplicates = true;
 }
 
-// Called when the game starts or when spawned
 void ATOCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	SetupLeaderPoseComponents();
+	UpdateSeatedStateVisuals();
 }
 
-// Called every frame
 void ATOCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
 void ATOCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
+void ATOCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ATOCharacter, bIsSeated);
+}
+
+void ATOCharacter::SetupLeaderPoseComponents()
+{
+	USkeletalMeshComponent* MainMesh = GetMesh();
+	if (!MainMesh) return;
+
+	// Automatically scan and sync all child USkeletalMeshComponents dynamically created in Blueprint (e.g. shorts, hair, top) under MainMesh
+	TArray<USceneComponent*> ChildComponents;
+	MainMesh->GetChildrenComponents(true, ChildComponents);
+	for (USceneComponent* Child : ChildComponents)
+	{
+		if (USkeletalMeshComponent* SkelChild = Cast<USkeletalMeshComponent>(Child))
+		{
+			if (SkelChild != MainMesh)
+			{
+				SkelChild->SetLeaderPoseComponent(MainMesh);
+			}
+		}
+	}
+}
+
+void ATOCharacter::RegisterModularMesh(USkeletalMeshComponent* ModularMesh)
+{
+	USkeletalMeshComponent* MainMesh = GetMesh();
+	if (ModularMesh && MainMesh && ModularMesh != MainMesh)
+	{
+		ModularMesh->SetLeaderPoseComponent(MainMesh);
+	}
+}
+
+void ATOCharacter::SetSeatedState(bool bSeated)
+{
+	bIsSeated = bSeated;
+	UpdateSeatedStateVisuals();
+}
+
+void ATOCharacter::OnRep_IsSeated()
+{
+	UpdateSeatedStateVisuals();
+}
+
+void ATOCharacter::UpdateSeatedStateVisuals()
+{
+	if (bIsSeated)
+	{
+		if (GetCharacterMovement())
+		{
+			GetCharacterMovement()->SetMovementMode(MOVE_None);
+		}
+		if (GetCapsuleComponent())
+		{
+			GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		}
+		if (SitIdleMontage && GetMesh() && GetMesh()->GetAnimInstance())
+		{
+			GetMesh()->GetAnimInstance()->Montage_Play(SitIdleMontage);
+		}
+	}
+	else
+	{
+		if (GetCharacterMovement())
+		{
+			GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+		}
+		if (GetCapsuleComponent())
+		{
+			GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		}
+	}
+}
+
+void ATOCharacter::AttachActorToHandSocket(AActor* TargetActor)
+{
+	if (!TargetActor) return;
+
+	if (GetMesh() && GetMesh()->DoesSocketExist(HandCardSocketName))
+	{
+		TargetActor->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, HandCardSocketName);
+	}
+	else if (GetMesh())
+	{
+		TargetActor->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+	}
+}
+
+void ATOCharacter::PlayEmote(ECharacterEmoteState EmoteState)
+{
+	if (HasAuthority())
+	{
+		Multicast_PlayEmote(EmoteState);
+	}
+	else
+	{
+		Multicast_PlayEmote(EmoteState);
+	}
+}
+
+void ATOCharacter::Multicast_PlayEmote_Implementation(ECharacterEmoteState EmoteState)
+{
+	UAnimInstance* AnimInst = GetMesh() ? GetMesh()->GetAnimInstance() : nullptr;
+	if (!AnimInst) return;
+
+	UAnimMontage* TargetMontage = nullptr;
+	switch (EmoteState)
+	{
+	case ECharacterEmoteState::Thinking:
+		TargetMontage = ThinkingMontage;
+		break;
+	case ECharacterEmoteState::SubmitAnswer:
+		TargetMontage = SubmitMontage;
+		break;
+	case ECharacterEmoteState::CorrectAnswer:
+		TargetMontage = CorrectMontage;
+		break;
+	case ECharacterEmoteState::WrongAnswer:
+		TargetMontage = WrongMontage;
+		break;
+	case ECharacterEmoteState::GuessSuccess:
+		TargetMontage = GuessSuccessMontage;
+		break;
+	default:
+		break;
+	}
+
+	if (TargetMontage)
+	{
+		AnimInst->Montage_Play(TargetMontage);
+	}
+}
--- Source/Operator/Character/TOCharacter.h
+++ Source/Operator/Character/TOCharacter.h
@@ -6,24 +6,95 @@
 #include "GameFramework/Character.h"
 #include "TOCharacter.generated.h"
 
+class UAnimMontage;
+class USkeletalMeshComponent;
+
+UENUM(BlueprintType)
+enum class ECharacterEmoteState : uint8
+{
+	None            UMETA(DisplayName = "None"),
+	Thinking        UMETA(DisplayName = "Thinking"),
+	SubmitAnswer    UMETA(DisplayName = "Submit Answer"),
+	CorrectAnswer   UMETA(DisplayName = "Correct Answer (+3)"),
+	WrongAnswer     UMETA(DisplayName = "Wrong Answer"),
+	GuessSuccess    UMETA(DisplayName = "Guess Success (+1)"),
+	Defeat          UMETA(DisplayName = "Defeat")
+};
+
 UCLASS()
 class OPERATOR_API ATOCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ATOCharacter();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// =========================================================================
+	// Dynamic Modular Mesh Leader Pose Sync
+	// =========================================================================
+	UFUNCTION(BlueprintCallable, Category = "Operator|ModularMesh")
+	void SetupLeaderPoseComponents();
+
+	UFUNCTION(BlueprintCallable, Category = "Operator|ModularMesh")
+	void RegisterModularMesh(USkeletalMeshComponent* ModularMesh);
+
+	// =========================================================================
+	// Seating State & Logic
+	// =========================================================================
+	UPROPERTY(ReplicatedUsing = OnRep_IsSeated, EditAnywhere, BlueprintReadWrite, Category = "Operator|Seating")
+	bool bIsSeated = true;
+
+	UFUNCTION(BlueprintCallable, Category = "Operator|Seating")
+	void SetSeatedState(bool bSeated);
+
+protected:
+	UFUNCTION()
+	void OnRep_IsSeated();
+
+	void UpdateSeatedStateVisuals();
+
+public:
+	// =========================================================================
+	// Hand Socket Card Attachment
+	// =========================================================================
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Operator|Card")
+	FName HandCardSocketName = TEXT("hand_r_Socket");
+
+	UFUNCTION(BlueprintCallable, Category = "Operator|Card")
+	void AttachActorToHandSocket(AActor* TargetActor);
+
+	// =========================================================================
+	// Animation Montages & Directing Emotes
+	// =========================================================================
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Operator|Animation")
+	UAnimMontage* SitIdleMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Operator|Animation")
+	UAnimMontage* ThinkingMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Operator|Animation")
+	UAnimMontage* SubmitMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Operator|Animation")
+	UAnimMontage* CorrectMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Operator|Animation")
+	UAnimMontage* WrongMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Operator|Animation")
+	UAnimMontage* GuessSuccessMontage;
+
+	UFUNCTION(BlueprintCallable, Category = "Operator|Animation")
+	void PlayEmote(ECharacterEmoteState EmoteState);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_PlayEmote(ECharacterEmoteState EmoteState);
 };
