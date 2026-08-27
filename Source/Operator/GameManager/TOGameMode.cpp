0564102ed492f7f64698ebe6dffc324e594ca5c5
kimik17 <f17kimik@gmail.com>
2026-08-27 16:13:02 +0900

    게임서버 코딩

--- Source/Operator/GameManager/TOGameMode.cpp
+++ Source/Operator/GameManager/TOGameMode.cpp
@@ -2,4 +2,75 @@
 
 
 #include "TOGameMode.h"
+#include "../Operation/TOCardDeckComponent.h"
+#include "../Operation/TOFormula.h"
 
+// 생성자
+ATOGameMode::ATOGameMode()
+{
+	CardDeckComponent = CreateDefaultSubobject<UTOCardDeckComponent>(TEXT("CardDeckComponent"));
+	CurrentGamePhase = ETOGamePhase::WaitingToStart;
+}
+
+// 라운드 시작 함수
+void ATOGameMode::StartNewRound(int32 PlayerCount)
+{
+	if (!CardDeckComponent) return;
+
+	// 1. 상태를 '수식 제출 단계'로 변경
+	CurrentGamePhase = ETOGamePhase::SubmittingFormulas;
+	SubmittedFormulasThisTurn.Empty();
+
+	// 2. 공개된 알파벳 목록 초기화
+	PlayerRevealedAlphabets.Empty();
+	for (int32 i = 0; i < PlayerCount; i++)
+	{
+		FPlayerAlphabetList NewList;
+		PlayerRevealedAlphabets.Add(i, NewList);
+       
+		// 점수판이 비어있다면 초기화
+		if (!PlayerScores.Contains(i))
+		{
+			PlayerScores.Add(i, 0);
+		}
+	}
+
+	// 3. 카드덱에서 새로운 수식 데이터 생성
+	CurrentFormulaData = CardDeckComponent->GenerateRoundFormula(PlayerCount);
+}
+
+// 플레이어가 수식을 제출했을 때
+void ATOGameMode::SubmitPlayerFormula(int32 PlayerIndex, const FString& SubmittedFormula)
+{
+	// 수식 제출 페이즈가 아니면 무시
+	if (CurrentGamePhase != ETOGamePhase::SubmittingFormulas) return;
+
+	// 이번 턴 제출 목록에 저장
+	SubmittedFormulasThisTurn.Add(PlayerIndex, SubmittedFormula);
+
+	// 모든 플레이어가 다 냈는지 검사하는 함수 호출
+	// CheckAllFormulasSubmitted();
+}
+
+// [Phase 2] 타인의 카드 유추 제출
+void ATOGameMode::SubmitPlayerGuess(int32 SourcePlayerIndex, int32 TargetPlayerIndex, const FString& GuessedValue)
+{
+	// TODO: 유추 정답 체크 및 점수/알파벳 공개 로직 구현
+}
+
+// 특정 플레이어에게 전달할 UI 데이터 반환
+FTOPlayerUIData ATOGameMode::GetUIDataForPlayer(int32 TargetPlayerIndex)
+{
+	FTOPlayerUIData UIData;
+	// TODO: UI 데이터 채워서 반환
+	return UIData;
+}
+
+// 다른 플레이어의 카드 유추 성공 시 알파벳 추가
+void ATOGameMode::AddRevealedAlphabetForPlayer(int32 TargetPlayerIndex, const FString& Alphabet)
+{
+	if (PlayerRevealedAlphabets.Contains(TargetPlayerIndex))
+	{
+		PlayerRevealedAlphabets[TargetPlayerIndex].Alphabets.AddUnique(Alphabet);
+	}
+}
--- Source/Operator/GameManager/TOPlayerState.h
+++ Source/Operator/GameManager/TOPlayerState.h
@@ -1,17 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
 #include "TOPlayerState.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class OPERATOR_API ATOPlayerState : public APlayerState
 {
 	GENERATED_BODY()
-	
+
+public:
+	// 선택한 캐릭터 ID
+	UPROPERTY(Replicated, BlueprintReadWrite, Category = "Character")
+	int32 SelectedCharacterID = -1;
+
+	// 캐릭터 선택 및 준비 완료 여부
+	UPROPERTY(Replicated, BlueprintReadWrite, Category = "Character")
+	bool bIsReadyToPlay = false;
+
+	// 멀티플레이어 동기화를 위한 변수 등록 함수
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 };
